2e4b5d9fba409d59f64363ab9aa1f8ebc110a925
Raadad Elsleiman <you@example.com>
2014-06-24 16:53:34 +1000

    updating for new version of node

--- entry.js
+++ entry.js
@@ -1,25 +1,25 @@
 /*
 *******************************************************
 * entry.js
-* 
+*
 * Author: Raadad Elsleiman
 *
 * Entrypoint for node-workout-robot web application
 *******************************************************
 */
 
-var Sequelize = require("sequelize");  // This is popular ORM library 
+var Sequelize = require("sequelize");  // This is popular ORM library
 var express = require('express')  // a minimalstic web application framework
 var app = express();  // creates a new instance of Express
 
 var sql = new Sequelize('workoutbot', 'vagruser' , 'devman'); //Creates a new instance that will allow access to our database
 
 
-/* The definition of our Exercise model, which will be stored and retrieved from our database, 
+/* The definition of our Exercise model, which will be stored and retrieved from our database,
 this Model will provide details of what exercises can be included into  a workout
 */
 
-var ExerciseModel = sql.define('exercise',{ 
+var ExerciseModel = sql.define('exercise',{
 	name:Sequelize.STRING,
 	time:Sequelize.INTEGER,
 	part:Sequelize.STRING,
@@ -38,27 +38,27 @@ var init = function(){
 		.success(function(){
 			var chainer = new Sequelize.Utils.QueryChainer
 			chainer
-				.add(ExerciseModel.create({			    
-						name: "Push Ups",  
+				.add(ExerciseModel.create({
+						name: "Push Ups",
 					    time: 4,
 					    part: "Arms"
 					}))
-				.add(ExerciseModel.create({			    
+				.add(ExerciseModel.create({
 						name: "Sit Ups",
 					    time: 3,
 					    part: "Abs"
 					}))
-				.add(ExerciseModel.create({			    
+				.add(ExerciseModel.create({
 						name: "Chin Ups",
 					    time: 20,
 					    part: "Arms"
 					}))
-				.add(ExerciseModel.create({			    
+				.add(ExerciseModel.create({
 						name: "Leg Raises",
 					    time: 5,
 					    part: "Abs"
 					}))
-				.add(ExerciseModel.create({			    
+				.add(ExerciseModel.create({
 						name: "Lunges",
 					    time: 6,
 					    part: "Legs"
@@ -68,15 +68,15 @@ var init = function(){
 	};
 
 
-/* 
-Express Web application configuration, 
+/*
+Express Web application configuration,
 */
 
 app.configure(function (){
-	app.set('views', __dirname+"/views"); 
+	app.set('views', __dirname+"/views");
 	app.set('view engine','jade')  // our page is composed using the jade templating engine
 	app.use(express.bodyParser());
-	app.set("jsonp callback",true); 
+	app.set("jsonp callback",true);
 
 	app.get("/", function(req,res){res.render("index")} );
 	app.get("/exercises/", function(req,res){
@@ -90,7 +90,7 @@ app.configure(function (){
 	app.get("/exercises/:id" , function(req,res){
 			ExerciseModel.find(req.params.id).success(function(data){
 				res.jsonp(data);
-			});			
+			});
 		});
 	app.use(app.router);
 	app.use("/" ,express.static(__dirname + '/public/')); //allows for serving of static files
@@ -100,8 +100,8 @@ if(process.argv[2] === "init"){
 	init();
 }
 else if(process.argv[2] === "start"){
-	app.listen(3200);
-	console.log("Starting Server listening for http on port 3200");
+	app.listen(8082);
+	console.log("Starting Server listening for http on port 8082");
 }
 else {
 	console.log("Arguments are:");
